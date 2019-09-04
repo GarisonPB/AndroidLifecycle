a2e97b53d8e1c5aeaf4bc1b08bf6efade8ec7183
GarisonPB <muhammadgarison99@gmail.com>
2019-09-04 20:46:58 +0700

    menambahkan kode untuk aplikasi tebak angka

--- AndroidLifecycle/app/src/main/java/id/ac/polinema/androidlifecycle/MainActivity.java
+++ AndroidLifecycle/app/src/main/java/id/ac/polinema/androidlifecycle/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
 	@Override
 	protected void onStart() {
 		super.onStart();
-		Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
+		Toast.makeText(this, "App On Start", Toast.LENGTH_SHORT).show();
 	}
 
 
@@ -27,33 +27,31 @@ public class MainActivity extends AppCompatActivity {
 	@Override
 	protected void onStop() {
 		super.onStop();
-		Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
+		Toast.makeText(this, "App On Stop", Toast.LENGTH_SHORT).show();
 	}
 
 	@Override
 	protected void onRestart() {
 		super.onRestart();
-		Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
+		Toast.makeText(this, "App On Restart", Toast.LENGTH_SHORT).show();
 	}
 	
 	@Override
 	protected void onResume() {
 		super.onResume();
-		Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
+		Toast.makeText(this, "App On Resume", Toast.LENGTH_SHORT).show();
 	}
 	
 	@Override
 	protected void onPause() {
 		super.onPause();
-		Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
+		Toast.makeText(this, "App On Pause", Toast.LENGTH_SHORT).show();
 	}
 
 	@Override
 	protected  void onDestroy() {
 		super.onDestroy();
-		Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
+		Toast.makeText(this, "App On Destroy", Toast.LENGTH_SHORT).show();
 	}
 
-
-	// TODO: lengkapi callback untuk onRestart() onResume() onPause() onDestroy()
 }
